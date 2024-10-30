0015b03fb291ceed94667deb180ade46666e4ceb
Krishna jatav <139641317+bcreativex@users.noreply.github.com>
2024-10-30 23:24:46 +0530

    Create hash.py

---
+++ hash.py
@@ -0,0 +1,12 @@
+hash_map = {}
+
+# Insertion
+hash_map['name'] = 'Alice'
+hash_map['age'] = 25
+
+# Access
+print(hash_map['name'])  # Output: Alice
+
+# Deletion
+del hash_map['age']
+print(hash_map)  # Output: {'name': 'Alice'}
